a0a1bf44c68194dbaba5166f1c3c63d9504a679b
rymnc <43716372+rymnc@users.noreply.github.com>
2023-11-11 22:55:38 +0300

    feat: rc-binding in o1js

--- rc-o1js/src/rc-binding.ts
+++ rc-o1js/src/rc-binding.ts
@@ -5,7 +5,7 @@ function assertPositiveInteger(n: number, message: string) {
 }
 
 import { rcConstants } from "./constants";
-import { FiniteField, Fp } from "./finite-field";
+import { FiniteField, Fp, mod } from "./finite-field";
 import { GroupMapPallas } from "./elliptic-curve";
 
 export { ReinforcedConcrete };
@@ -55,12 +55,134 @@ function createRc(
   }
 
   function hash(input: bigint[]) {
-    let state = permutation(input);
+    if(input.length !== 2) throw Error(`Input must be 2 elements long, got ${input.length}`);
+    
+    let state = permutation(input.concat(0n));
     return state[0];
   }
 
+  function concrete(state: bigint[], i: number) {
+    //     sum = state[0] + state[1] + state[2]; 
+    
+    //     outState[0] = sum + state[0] + rcConstantA.out;
+    //     outState[1] = sum + state[1] + rcConstantB.out;
+    //     outState[2] = sum + state[2] + rcConstantC.out;
+    
+    // }
+    const sum = Fp.add(Fp.add(state[0], state[1]), state[2]);
+
+    const res: bigint[] = [];
+    for(let i=0; i<3; i++) {
+        res[i] = Fp.add(Fp.add(sum, state[i]), params.concreteConstants[i * 3 + i]);
+    }
+    return res;
+  }
+
+  function bricks(state: bigint[]) {
+    //     outState[0] = s0 ^ 5;
+    //     outState[1] = ((s0^2 + state[0]) + 2) * state[1];
+    //     outState[2] <== (s1^2 + (3 * state[1]) + 4) * state[2];
+
+    const res: bigint[] = [];
+
+    const state0Sq = Fp.mul(state[0], state[0]);
+    const state1Sq = Fp.mul(state[1], state[1]);
+
+    res[0] = Fp.mul(Fp.square(state0Sq), state[0]);
+    res[1] = Fp.mul(Fp.add(Fp.add(state0Sq, state[0]), 2n), state[1]);
+    res[2] = Fp.mul(Fp.add(Fp.add(state1Sq, Fp.mul(state[1], 3n)), 4n), state[2]);
+
+    return res;
+  }
+
+  function decompose(state: bigint) {
+    // recursively divide the state until we get 27 elements confined to p = 700
+
+
+    const divisors = params.divisors;
+    const res: bigint[] = [];
+    
+    let repr = state;
+    for(let i=26; i>0; i--) {
+        res[i] = mod(repr, divisors[i]);
+        let tmp = Fp.div(repr, divisors[i]);
+        if(tmp === undefined) throw new Error(`Division undefined! ${repr}/${divisors[i]}`);
+        repr = tmp;
+    }
+    res[0] = repr;
+
+    return res;
+  }
+
+  function sbox(decomposedValues: bigint[]) {
+    const res: bigint[] = [];
+
+    // sbox lookup
+    for(let i=0; i<decomposedValues.length; i++) {
+        res[i] = params.sbox[Number(decomposedValues[i])];
+    }
+
+    return res;
+  }
+
+  function compose(sboxedValues: bigint[]) {
+    // accumulate the sboxed values into 1 element
+
+    const res: bigint[] = [];
+
+    res[0] = sboxedValues[0];
+    for(let i = 1; i<27; i++) {
+        res[2*i-1] = Fp.mul(res[2*i-2], sboxedValues[i]);
+        res[2*i] = Fp.add(res[2*i-1], sboxedValues[i]);
+    }
+
+    return res[52];
+  }
+
+  function bars(state: bigint[]) {
+    const decomposedValues: bigint[][] = [];
+    const sboxedValues: bigint[][] = [];
+    const res: bigint[] = [];
+
+    // first decompose
+    for(let i=0; i<3; i++) {
+        decomposedValues[i] = decompose(state[i]);
+    }
+
+    // sbox lookup
+    for(let i=0; i<3; i++) {
+        sboxedValues[i] = sbox(decomposedValues[i]);
+    }
+
+    // then compose
+    for(let i=0; i<3; i++) {
+        res[i] = compose(sboxedValues[i]);
+    }
+
+    return res;
+  } 
 
   function permutation(state: bigint[]) {
+    let intermediateState: bigint[];
+    // concrete and bricks & concrete 3 times
+    intermediateState = concrete(state, 0);
+
+    for(let i=1; i<4; i++) {
+        intermediateState = bricks(intermediateState);
+        intermediateState = concrete(intermediateState, i);
+    }
+
+    // bars
+    intermediateState = bars(intermediateState);
+
+    // concrete & bricks 3 times and concrete
+    intermediateState = concrete(intermediateState, 4);
+    for(let i=5; i<8; i++) {
+        intermediateState = bricks(intermediateState);
+        intermediateState = concrete(intermediateState, i);
+    }
+
+    return intermediateState;
   }
 
   return { initialState, hash };
